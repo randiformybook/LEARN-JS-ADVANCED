b0348034d9a60bc407b6e6e2515cbecd8476215a
Randi Himawan <124506252+randiformybook@users.noreply.github.com>
2024-06-26 10:35:24 +0700

    Perbuhaan

---
+++ THIS ON ARROW FUNCTION/scripts/script.bat
@@ -0,0 +1,20 @@
+// const DataKaryawan = function () {
+//   this.nama = "randi";
+//   this.umur = 35;
+//   this.sayHello = function () {
+//     console.log(`hai, saya ${this.nama}, dengan umur ${this.umur} tahun.`);
+//     console.log(this);
+//   };
+// };
+
+// const kyw1 = new DataKaryawan();
+
+const nama = "putri";
+
+const Pelajar = {
+  nama: "randi",
+  umur: 35,
+  sayHello: function () {
+    console.log(this);
+  },
+};
--- THIS ON ARROW FUNCTION/scripts/script.js
+++ THIS ON ARROW FUNCTION/scripts/script.js
@@ -1,20 +1,9 @@
-// const DataKaryawan = function () {
-//   this.nama = "randi";
-//   this.umur = 35;
-//   this.sayHello = function () {
-//     console.log(`hai, saya ${this.nama}, dengan umur ${this.umur} tahun.`);
-//     console.log(this);
-//   };
-// };
-
-// const kyw1 = new DataKaryawan();
-
-const nama = "putri";
-
-const Pelajar = {
-  nama: "randi",
-  umur: 35,
-  sayHello: function () {
-    console.log(this);
-  },
-};
+// Ambil semua element video
+const videos = document.querySelectorAll("li data-duration");
+console.log(videos);
+// Pilih yang hanya bertuliskan "JAVASCRIPT LANJUTAN"
+// Ambil durasi masing masing video
+// Ubah durasi menjadi int, menit menjadi detik
+// Jumlahkan semua detik
+// Ubah formatnya menjadi Jam,Menit dan Detik
+// Simpan ke DOM
