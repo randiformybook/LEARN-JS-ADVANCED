0f4947ac21583534c2a1c17f27fa0b115f8a12db
randiformybook <randi.mybook@gmail.com>
2024-06-26 14:56:42 +0700

    selesai

--- THIS ON ARROW FUNCTION/scripts/script.js
+++ THIS ON ARROW FUNCTION/scripts/script.js
@@ -1,9 +1,25 @@
 // Ambil semua element video
-const videos = document.querySelectorAll("li data-duration");
-console.log(videos);
+const videos = Array.from(document.querySelectorAll("[data-duration]"));
+
 // Pilih yang hanya bertuliskan "JAVASCRIPT LANJUTAN"
-// Ambil durasi masing masing video
-// Ubah durasi menjadi int, menit menjadi detik
-// Jumlahkan semua detik
-// Ubah formatnya menjadi Jam,Menit dan Detik
+const selectedVideos = Array.from(videos)
+  .filter((video) => video.textContent.includes("JAVASCRIPT LANJUTAN"))
+  // Ambil durasi masing masing video
+  .map((videoDuration) => videoDuration.dataset.duration)
+  // Ubah durasi menjadi int, menit menjadi detik
+  .map((durasi) => {
+    const [menit, detik] = durasi.split(":").map(Number);
+    return menit * 60 + detik;
+  })
+  // Jumlahkan semua detik
+  .reduce((total, detik) => total + detik, 0);
+// mengubah format  total detik menjadi format total waktu (jam:menit:detik)
+const totalWaktu = (totalDetik) => {
+  const jam = Math.floor(totalDetik / 3600);
+  const menit = Math.floor((totalDetik - jam * 3600) / 60);
+  const detik = totalDetik - jam * 3600 - menit * 60;
+  return `${jam}:${menit}:${detik}`;
+};
 // Simpan ke DOM
+const saveTotalDurasi = document.querySelector(".total-durasi");
+saveTotalDurasi.innerText = totalWaktu(selectedVideos);
