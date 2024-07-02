5e5b03df4eb7d7585b3c2f6b0eaf32c5f0c5caf9
randiformybook <randi.mybook@gmail.com>
2024-07-02 17:00:38 +0700

    yes

--- 0. GENERAL/script/script-1.js
+++ 0. GENERAL/script/script-1.js
@@ -1,4 +1,6 @@
 // HTML Elements
+// 1. Looping Array of Object dan di masukan kedalam HTML
+// --------------------------------------------
 // const Karyawan = [
 //   {
 //     nama: "Randi Himawan",
@@ -14,7 +16,8 @@
 //   },
 // ];
 
-// // Looping array dari karyawan
+// Looping array dari karyawan
+// --------------------------------------------
 // const el = `<div class="karyawan">
 //   ${Karyawan.map((k) => {
 //     return `<ul>
@@ -25,29 +28,69 @@
 // </div>`;
 
 // 3.Conditional
-const player = [
-  {
-    position: "Defender",
-    nama: "Sergio Ramos",
-    patner: "Pepe",
-  },
-  {
-    position: "Striker",
-    nama: "Benzema",
-  },
-];
+// --------------------------------------------
+// const player = [
+//   {
+//     position: "Defender",
+//     nama: "Sergio Ramos",
+//     patner: "Pepe",
+//   },
+//   {
+//     position: "Striker",
+//     nama: "Benzema",
+//   },
+// ];
+
+// const el = `<div class="player">
+//   ${player
+//     .map((pos) => {
+//       return `<ul>
+//       <li> Nama Pemain :${pos.nama}</li>
+//       <li>Posisi sebagai : ${pos.position} ${
+//         pos.patner ? `dan patnernya ${pos.patner}` : ``
+//       }</li>
+//     </ul>`;
+//     })
+//     .join("")}
+// </div>`;
+
+// 4.Membuat Function Sendiri
+// --------------------------------------------
+const dataPerusahaan = {
+  namaPerusahaan: "PT.Usaha Bersama",
+  tahun: 2020,
+  dataKaryawan: [
+    {
+      nama: "Randi Himawan",
+      id: 127000111,
+      posisi: "3D Artist",
+    },
+    {
+      nama: "Putri Swandayani",
+      id: 227000222,
+      posisi: "Dokter Umum",
+    },
+    {
+      nama: "Muhammad Ilyas",
+      id: 327000333,
+      posisi: "Pilot",
+    },
+  ],
+};
+
+function callofDataKarywan() {
+  return `<ol>${dataPerusahaan.dataKaryawan
+    .map((k) => {
+      return `<li>${k.nama} - ${k.id} - ${k.posisi}</br></li>`;
+    })
+    .join("")}</ol>`;
+}
 
 const el = `<div class="player">
-  ${player
-    .map((pos) => {
-      return `<ul>
-      <li> Nama Pemain :${pos.nama}</li>
-      <li>Posisi sebagai : ${pos.position} ${
-        pos.patner ? `dan patnernya ${pos.patner}` : ``
-      }</li>
-    </ul>`;
-    })
-    .join("")}
+<h2>Perusahaan : ${dataPerusahaan.namaPerusahaan}</h2>
+<h3><i>Berdiri dari Tahun : ${dataPerusahaan.tahun}</i></h3>
+<span>Data Karyawan :</span>
+<span>${callofDataKarywan()}</span>
 </div>`;
 
 document.body.innerHTML = el;
