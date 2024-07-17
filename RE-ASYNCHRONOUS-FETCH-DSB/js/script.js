756852414fa3f571d703495ef6ff1f662252c6f0
Randi Himawan <randi.mybook@gmail.com>
2024-07-17 22:15:31 +0700

    "Updated HTML and JavaScript code for asynchronous fetch and display of movie details"

--- RE-ASYNCHRONOUS-FETCH-DSB/js/script.js
+++ RE-ASYNCHRONOUS-FETCH-DSB/js/script.js
@@ -1,27 +1,67 @@
 $.ajax({
-  url: "http://www.omdbapi.com/?i=tt3896198&apikey=1cf40ec1&s=avengers",
+  url: "http://www.omdbapi.com/?apikey=1cf40ec1&s=avengers",
   success: (results) => {
     const movies = results.Search;
     let cards = "";
     movies.forEach((movie) => {
-      cards += cardsResult(movie);
+      cards += showCard(movie);
     });
     $(".card-container").html(cards);
+
+    // Ketika Button Show Details di Click
+    $(".detailButton").on("click", function () {
+      console.log($(this).data("imdbid"));
+      $.ajax({
+        url:
+          "http://www.omdbapi.com/?apikey=1cf40ec1&i=" + $(this).data("imdbid"),
+        success: (mdetail) => {
+          let movieDetail = showDetails(mdetail);
+          $(".modal-body").html(movieDetail);
+        },
+      });
+    });
   },
   error: (e) => {
     console.error(e);
   },
 });
 
-function cardsResult(movie) {
+function showCard(movie) {
   return `<div class="col-md-3 my-4">
                     <div class="card">
                       <img src=${movie.Poster} class="card-img-top" alt="" />
                       <div class="card-body">
                         <h5 class="card-title">${movie.Title}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">${movie.Year}</h6>
-                        <a href="#" class="btn btn-primary"    data-bs-toggle="modal" data-bs-target="#movieDetails">Show Details</a>
+                        <a href="#" class="btn btn-primary detailButton" data-bs-toggle="modal" data-bs-target="#movieDetails" data-imdbid ="${movie.imdbID}">Show Details</a>
                       </div>
                     </div>
                   </div>`;
+}
+
+function showDetails(mdetail) {
+  return `<div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-3">
+                      <img src="${mdetail.Poster}" class="img-fluid" />
+                    </div>
+                    <div class="col-md">
+                      <ul class="list-group">
+                        <li class="list-group-item"><h4>${mdetail.Title} (${mdetail.Year})</h4></li>
+                        <li class="list-group-item">
+                          <strong>Director : </strong>${mdetail.Director}
+                        </li>
+                        <li class="list-group-item">
+                          <strong>Actors : </strong>${mdetail.Actors}
+                        </li>
+                        <li class="list-group-item">
+                          <strong>Writer :</strong>${mdetail.Writer}
+                        </li>
+                        <li class="list-group-item">
+                          <strong>Plot : </strong>${mdetail.Plot}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>`;
 }
